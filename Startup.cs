c55e672033327acb0d2d3e75c37cd07525fe9d08
alih0111 <heiidaryy@gmail.com>
2022-12-11 10:05:32 +0330

    init commit

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Metro.Startup))]
+namespace Metro
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
